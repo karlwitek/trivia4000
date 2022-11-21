bf8bb3ed8362a45f0b65ff43a9eb3d62a2b28383
karlwitek <karl.witek20@gmail.com>
2022-11-21 13:24:57 -0800

    home route

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 const router = require('./routes/router.js');
 
+const pool = require('./db');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 // app.use('/', router);
 
-
+app.use('/', (req, res) => {
+  pool.query('SELECT * FROM users', (err, result) => {
+    if (err) { console.log(err.stack); }
+    res.send(result.rows);
+  });
+});
 
 app.listen(PORT, console.log(`server running on port: ${PORT}`));
