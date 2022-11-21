b86870adbb90c1860ea3975a79257bcadff73e2f
karlwitek <karl.witek20@gmail.com>
2022-11-21 09:48:50 -0800

    update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const router = require('./routes/router.js');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/', router);
+
+app.listen(PORT, console.log(`server running on port: ${PORT}`));
