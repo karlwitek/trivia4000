698feec89e3673de9a20b07ce829f23ce812d7e1
karlwitek <karl.witek20@gmail.com>
2022-11-21 13:19:47 -0800

    add schema

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use('/', router);
+// app.use('/', router);
+
+
 
 app.listen(PORT, console.log(`server running on port: ${PORT}`));
---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  username text NOT NULL,
+  userpassword text NOT NULL,
+  totalGuesses integer DEFAULT 0,
+  correct integer DEFAULT 0,
+  incorrect integer DEFAULT 0,
+  numGamesPlayed integer DEFAULT 0,
+  active boolean DEFAULT false
+);
+
