7a6ad05f789ecf64c7facbdb9baa039dc516ca00
karlwitek <karl.witek20@gmail.com>
2022-11-22 13:18:12 -0800

    local port number change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const router = require('./routes/router.js');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 dotenv.config({ path: './config/config.env'});
