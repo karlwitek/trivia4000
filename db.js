6e282dda394a066767061b6b80fd5591f75925d4
karlwitek <karl.witek20@gmail.com>
2022-11-22 15:02:10 -0800

    use connection string in db

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//   host: 'trivia4000.herokuapp.com',
+//   user: 'karl',
+//   database: 'players',
+//   password: 'karl123',
+//   port: 5432,
+// });
+
+const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
-  host: 'trivia4000.herokuapp.com',
-  user: 'karl',
-  database: 'players',
-  password: 'karl123',
-  port: 5432,
+  connectionString,
 });
 
 module.exports = pool;
