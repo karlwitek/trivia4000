9951113e000973b3935870d5a2ff6770e21e32c4
karlwitek <karl.witek20@gmail.com>
2022-11-22 16:11:08 -0800

    db file

--- db.js
+++ db.js
@@ -1,16 +1,18 @@
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-//   host: 'localhost',
-//   user: 'karl',
-//   database: 'players',
-//   password: 'karl123',
-//   port: 5432,
-// });
-
-const connectionString = process.env.DATABASE_URL;
+if (process.env.NODE_ENV === 'development') {
+  const pool = new Pool({
+    host: 'localhost',
+    user: 'karl',
+    database: 'players',
+    password: 'karl123',
+    port: 5432,
+  });
+} else {
+  const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
   connectionString,
 });
+}
 
 module.exports = pool;
