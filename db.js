1f9ca4a92afb4c519b161ff3e178e1fae5d1a5d2
karlwitek <karl.witek20@gmail.com>
2022-11-23 10:57:47 -0800

    db file change

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 const { Pool } = require('pg');
 
 
-const pool = new Pool({
-    host: 'localhost',
-    user: 'karl',
-    database: 'players',
-    password: 'karl123',
-    port: 5432,
-  });
+// const pool = new Pool({
+//     host: 'localhost',
+//     user: 'karl',
+//     database: 'players',
+//     password: 'karl123',
+//     port: 5432,
+//   });
 
-  // const connectionString = process.env.DATABASE_URL;
-  // const pool = new Pool({
-  //   connectionString,
-  // });
+  const connectionString = process.env.DATABASE_URL;
+  const pool = new Pool({
+    connectionString,
+  });
 
 
 // let pool;
