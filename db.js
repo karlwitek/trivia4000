f92ba71e624ed71a8b7b7c82db72ac55bed2f892
karlwitek <karl.witek20@gmail.com>
2022-11-22 16:05:21 -0800

    db file

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-//   host: 'trivia4000.herokuapp.com',
-//   user: 'karl',
-//   database: 'players',
-//   password: 'karl123',
-//   port: 5432,
-// });
-
-const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
-  connectionString,
+  host: 'localhost',
+  user: 'karl',
+  database: 'players',
+  password: 'karl123',
+  port: 5432,
 });
+
+// const connectionString = process.env.DATABASE_URL;
+// const pool = new Pool({
+//   connectionString,
+// });
 
 module.exports = pool;
