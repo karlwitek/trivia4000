3fcd1bb45ef030975cf95668eb1e4a7a647cf5b5
karlwitek <karl.witek20@gmail.com>
2022-11-23 11:19:58 -0800

    db file

--- db.js
+++ db.js
@@ -12,6 +12,9 @@ const { Pool } = require('pg');
   const connectionString = process.env.DATABASE_URL;
   const pool = new Pool({
     connectionString,
+    ssl: {
+      rejectUnauthorized: false
+    }
   });
 
 
